d3e994adfe02916bdc7a9ee3dc48d047d78e37b3
Igor Nicoletti <igornicoletti@outlook.com>
2024-07-19 23:33:04 -0300

    updated component

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { SwitchComponent } from './components'
 
 export const App = () => {
   return (
-    <div className={'w-full min-h-screen grid overflow-x-hidden text-[#f8f8f2] bg-gradient-to-r from-sky-500 to-cyan-500 dark:from-blue-500 dark:to-indigo-500'}>
+    <div className={'w-full min-h-screen grid overflow-x-hidden bg-gradient-to-r from-sky-500 to-cyan-500 dark:from-blue-500 dark:to-indigo-500 text-[#f8f8f2]'}>
       <div className={'w-full h-full flex flex-col items-center justify-center gap-4 px-6 py-8'}>
         <h1 className={'font-medium text-center uppercase tracking-widest'}>Theme LocalStorage</h1>
         <SwitchComponent />
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -4,18 +4,15 @@ import { Switch } from '@headlessui/react'
 import { UseLocalStorage } from '../hooks'
 
 export const SwitchComponent = () => {
-  const [isToggled, setToggle] = UseLocalStorage('@theme-toggle', true)
+  const [isToggled, setToggle] = UseLocalStorage('@theme-toggle', false)
 
-  useEffect(() => {
-    isToggled
-      ? document.documentElement.classList.add('dark')
-      : document.documentElement.classList.remove('dark')
-  }, [isToggled])
+  useEffect(() => isToggled
+    ? document.documentElement.classList.add('dark')
+    : document.documentElement.classList.remove('dark'), [isToggled])
 
   return (
-    <Switch checked={isToggled} onChange={setToggle} className={'relative inline-flex h-10 w-20 shrink-0 cursor-pointer rounded-full bg-black/25 border-2 border-transparent focus:outline-none'}>
-      <span className={'sr-only'}>Theme LocalStorage</span>
-      <span aria-hidden={true} className={`${isToggled ? 'translate-x-10' : 'translate-x-0'} pointer-events-none inline-block h-9 w-9 transform rounded-full bg-white shadow-lg ring-0 transition duration-200 ease-in-out`} />
+    <Switch checked={isToggled} onChange={setToggle} className={'h-8 w-16 group flex rounded-full focus:outline-none border-4 border-black/5 bg-black/10'}>
+      <span aria-hidden={true} className={'h-6 w-8 rounded-full transform translate-x-0 group-aria-checked:translate-x-6 transition ease-in-out duration-200 bg-white'} />
     </Switch>
   )
 }
